5e8fc50bcd0e3727b3baf1f7f10ef3bd4d331e5a
javigh193 <115082160+javigh193@users.noreply.github.com>
2023-12-11 15:31:13 +0100

    Listo para entregar

--- ut5/juego_eventos_teclado/script.js
+++ ut5/juego_eventos_teclado/script.js
@@ -1,3 +1,35 @@
+// Funciónes para cronometrar la partida
+
+// Dados segundos transcurridos, devuelve en formato 'hh:mm:ss'
+function convertTime(seconds) {
+    let hAux, mAux, sAux;
+    let h = Math.floor(seconds / 3600);
+    let m = Math.floor((seconds % 3600) / 60);
+    let s = (seconds % 3600) % 60
+    s < 10 ? sAux = "0" + s : sAux = s;
+    m < 10 ? mAux = "0" + m : mAux = m;
+    h < 10 ? hAux = "0" + h : hAux = h;
+    return `${hAux}:${mAux}:${sAux}`;
+}
+
+function startCrono() {
+    total_sec = 0; // Resolución de segundos
+    document.getElementById("crono").innerHTML="00:00:00";
+    sec_count = setInterval(updateCrono,1000);
+}         
+
+function updateCrono() {
+    total_sec++;
+    document.getElementById("crono").innerHTML = convertTime(total_sec); 
+}
+
+function stopCrono() {
+    clearInterval(sec_count);
+}
+
+// Se inicializa mejor tiempo en la sesión, si no existe ya
+if (sessionStorage.getItem("best_time") === null) {sessionStorage.setItem("best_time", "0")}
+
 // Definición de la clase Ball para representar la bola en el juego
 class Ball {
     constructor(ctx, x, y, radius, dx, dy) {
@@ -60,6 +92,7 @@ class Brick {
 // Definición de la clase Game para manejar la lógica del juego
 class Game {
     constructor(canvasId) {
+        this.paused = false; // Se añade un atributo 'paused' para cambiar el estado de la partida
         this.canvas = document.getElementById(canvasId); // Elemento canvas en el HTML
         this.ctx = this.canvas.getContext("2d"); // Contexto de dibujo en el lienzo
         this.ball = new Ball(this.ctx, this.canvas.width / 2, this.canvas.height - 30, 10, 2, -2); //
@@ -75,6 +108,7 @@ class Game {
         document.addEventListener("keyup", this.keyUpHandler.bind(this), false);
         // Llamar al método draw repetidamente cada 10 milisegundos
         setInterval(this.draw.bind(this), 10);
+        startCrono();
     }
 
     // Método para crear la matriz de instancias de ladrillos
@@ -98,6 +132,12 @@ class Game {
             this.rightPressed = true;
         } else if (e.keyCode == 37) {
             this.leftPressed = true;
+        } else if (e.keyCode == 38) { // ArrowUp aumentar velocidad 
+            this.ball.dx *= 1.1;
+            this.ball.dy *= 1.1;
+        } else if (e.keyCode == 40) { // ArrowDown reduce velocidad
+            this.ball.dx *= 0.9;
+            this.ball.dy *= 0.9;
         }
     }
 
@@ -122,8 +162,30 @@ class Game {
                         b.status = 0;
                         this.score++;
                         if (this.score === 15) {
-                            alert("¡GANASTE, FELICIDADES!");
-                            window.location.reload();
+                            stopCrono(); // Se detiene el crono
+                            let best_time = parseInt(sessionStorage.getItem("best_time")); // Se carga el mejor tiempo anterior como entero
+                            if (total_sec < best_time || best_time == 0) { // Se compara el resultado con el récord guardado
+                                if (best_time != 0) { // El resultado pasa a ser nuevo récord
+                                    let last_record = convertTime(best_time); 
+                                    best_time = total_sec;
+                                    sessionStorage.setItem("best_time", `${best_time}`);
+                                    alert(`¡GANASTE, FELICIDADES!"\n
+                                    Nuevo récord: ${convertTime(best_time)}\n
+                                    Récord anterior: ${last_record}`);
+                                    window.location.reload();
+                                } else { // Es el primer resultado registrado, se guarda como récord
+                                    best_time = total_sec;
+                                    sessionStorage.setItem("best_time", `${best_time}`);
+                                    alert(`¡GANASTE, FELICIDADES!"\n
+                                    Nuevo récord: ${convertTime(best_time)}`);
+                                    window.location.reload();
+                                }
+                            } else { // El resultado actual no es récord
+                                alert(`¡GANASTE, FELICIDADES!"\n
+                                Resultado actual: ${convertTime(total_sec)}\n
+                                Mejor resultado: ${convertTime(best_time)}`);
+                                window.location.reload();
+                            }
                         }
                     }
                 }
@@ -133,42 +195,45 @@ class Game {
 
     // Método principal para dibujar y actualizar el juego
     draw() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.drawBricks();
-        this.ball.draw();
-        this.paddle.draw();
-        this.drawScore();
-        this.drawLives();
-        this.collisionDetection();
-        if (this.ball.x + this.ball.dx > this.canvas.width - this.ball.radius || this.ball.x + this.ball.dx < this.ball.radius) {
-            this.ball.dx = -this.ball.dx;
-        }
-        if (this.ball.y + this.ball.dy < this.ball.radius) {
-            this.ball.dy = -this.ball.dy;
-        } else if (this.ball.y + this.ball.dy > this.canvas.height - this.ball.radius) {
-            if (this.ball.x > this.paddle.x && this.ball.x < this.paddle.x + this.paddle.width) {
+        if (!this.paused) { //se actualiza el estado del juego cuando no está pausado
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.drawBricks();
+            this.ball.draw();
+            this.paddle.draw();
+            this.drawScore();
+            this.drawLives();
+            this.collisionDetection();
+            if (this.ball.x + this.ball.dx > this.canvas.width - this.ball.radius || this.ball.x + this.ball.dx < this.ball.radius) {
+                this.ball.dx = -this.ball.dx;
+            }
+            if (this.ball.y + this.ball.dy < this.ball.radius) {
                 this.ball.dy = -this.ball.dy;
-            } else {
-                this.lives--;
-                if (!this.lives) {
-                    alert("¡JUEGO TERMINADO!");
-                    window.location.reload();
+            } else if (this.ball.y + this.ball.dy > this.canvas.height - this.ball.radius) {
+                if (this.ball.x > this.paddle.x && this.ball.x < this.paddle.x + this.paddle.width) {
+                    this.ball.dy = -this.ball.dy;
                 } else {
-                    this.ball.x = this.canvas.width / 2;
-                    this.ball.y = this.canvas.height - 30;
-                    this.ball.dx = 2;
-                    this.ball.dy = -2;
-                    this.paddle.x = (this.canvas.width - this.paddle.width) / 2;
+                    this.lives--;
+                    if (!this.lives) {
+                        stopCrono();
+                        alert('JUEGO TERMINADO!');
+                        window.location.reload();
+                    } else {
+                        this.ball.x = this.canvas.width / 2;
+                        this.ball.y = this.canvas.height - 30;
+                        this.ball.dx = 2;
+                        this.ball.dy = -2;
+                        this.paddle.x = (this.canvas.width - this.paddle.width) / 2;
+                    }
                 }
             }
+            if (this.rightPressed && this.paddle.x < this.canvas.width - this.paddle.width) {
+                this.paddle.x += 7;
+            } else if (this.leftPressed && this.paddle.x > 0) {
+                this.paddle.x -= 7;
+            }
+            this.ball.x += this.ball.dx;
+            this.ball.y += this.ball.dy;
         }
-        if (this.rightPressed && this.paddle.x < this.canvas.width - this.paddle.width) {
-            this.paddle.x += 7;
-        } else if (this.leftPressed && this.paddle.x > 0) {
-            this.paddle.x -= 7;
-        }
-        this.ball.x += this.ball.dx;
-        this.ball.y += this.ball.dy;
     }
     
     // Método para dibujar los ladrillos en el lienzo
@@ -197,6 +262,18 @@ class Game {
     }
 }
 
+// Botón para pausar y reanudar el juego
+function pauseGame(e) {
+    if (game.paused) {
+        game.paused = false;
+        e.target.innerHTML = 'Pause';
+    } else {
+        game.paused = true;
+        e.target.innerHTML = 'Resume';
+    }
+}
+
+document.getElementById('pause-btn').addEventListener('click', pauseGame);
+
 // Crear instancia del juego
 let game = new Game("myCanvas");
-
